e4022e1e1458981dd37d511644a0f7c78ec38f0b
Rafael Sousa <rafmsou@gmail.com>
2012-11-12 00:05:38 -0200

    Lista Produto

---
+++ ListaProduto.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace GRC
+{
+    partial class ListaProduto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgProdutos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProdutos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgProdutos
+            // 
+            this.dgProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProdutos.Location = new System.Drawing.Point(43, 41);
+            this.dgProdutos.Name = "dgProdutos";
+            this.dgProdutos.Size = new System.Drawing.Size(437, 379);
+            this.dgProdutos.TabIndex = 0;
+            // 
+            // ListaProduto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 477);
+            this.Controls.Add(this.dgProdutos);
+            this.Name = "ListaProduto";
+            this.Text = "Lista de Produtos";
+            this.Load += new System.EventHandler(this.ListaProduto_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgProdutos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgProdutos;
+    }
+}
---
+++ ListaProduto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GRC
+{
+    public partial class ListaProduto : Form
+    {
+        public ListaProduto()
+        {
+            InitializeComponent();
+        }
+
+        private void ListaProduto_Load(object sender, EventArgs e)
+        {
+            GRCEntities db = new GRCEntities();
+            var produtos = from p in db.PRODUTO
+                           select new { 
+                            Codigo = p.CODIGO,
+                            Descricao = p.DESCRICAO,
+                            Preco_Unit = p.VALOR
+                           };
+
+            dgProdutos.DataSource = produtos;
+        }
+    }
+}
